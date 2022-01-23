d0d0c090f840b0d91464633fdbdb3213a039f631
Alex Ross <alxrg@me.com>
2022-01-23 12:44:22 -0600

    Dictionary Words Selection and randomized

--- tutorial/dictionary_words.py
+++ tutorial/dictionary_words.py
@@ -1,9 +1,9 @@
 import random
 import sys
 
-numbers = int(sys.argv[1])
+input_number = int(sys.argv[1])
 # Open and read the word file
-dictionary = open('./words-sample.txt', 'r')
+dictionary = open('./web2', 'r')
 
 word_lines = dictionary.read().splitlines()
 length_of_dict = len(word_lines)
@@ -15,11 +15,26 @@ dictionary.close()
 random_selection = []
 
 index = 0
-while index < numbers:
+while index < input_number:
   new_word = word_lines[random.randint(0,length_of_dict)]
   random_selection.append(new_word)
   index += 1
 
 # print(len(word_lines))
 new_random_sentence = ' '.join(random_selection)
-print(random_selection)
+print(new_random_sentence)
+
+#Argument for the file
+#Augmenting the dictionary words function 
+#
+
+# Small body of text 1, just 1 blog post, 
+# analyzing the frequency of words in any body of text
+# display the histogram. 
+# work on three functions 1st  function is a historgram taking in called file name sourcetext.txt
+# read in the file 
+# return unique histogram structure that returns the number of unique words within it. 
+# last Frequency function = take a word and the frequency itself
+# i.e. Mystery IN Sherlock holmes comes back 20 times.
+# Choose and do it TWO of the 3 ways of doing it. 
+# list, tuples or key-value pairs
